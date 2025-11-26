32f5e7e130c63c7f83d2e261c362de9e237181f9
testingpages15 <matt01253@hotmail.co.uk>
2025-11-26 15:21:05 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
-// Simple network-only Service Worker for the SPA
-// Safe version - will not break PWA install
+// Simple network-only service worker
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   console.log("SW installed");
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   console.log("SW activated");
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
